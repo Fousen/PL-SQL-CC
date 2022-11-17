643e5a4121960303256198d0e98225b47182de82
Fousen <80414662+Fousen@users.noreply.github.com>
2022-11-17 15:18:49 +0530

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,7 +1,7 @@
 create table part( 
   part_no    NUMBER(6), 
   part_name  VARCHAR2(20), 
-  in_stock   VARCHAR2(1), 
+  in_stock   NUMBER, 
   part_price NUMBER(6,2), 
   part_desc  VARCHAR2(50) 
 )
